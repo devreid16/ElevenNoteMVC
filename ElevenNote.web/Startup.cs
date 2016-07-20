e36c00a84db62cd2f8da59d158175a7aedd8fbf1
Angela Reid <reidang94@gmail.com>
2016-07-20 10:54:27 -0400

    initial commit

---
+++ ElevenNote.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.web.Startup))]
+namespace ElevenNote.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

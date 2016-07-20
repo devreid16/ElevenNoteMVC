a97ab9494b2c57e8c6afe1298d644c9099a69592
Angela Reid <reidang94@gmail.com>
2016-07-20 11:40:47 -0400

    added new class models

---
+++ ElevenNote.Model/NoteDetailModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Model
+{
+    public class NoteDetailModel
+    {
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTimeOffset CreateUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        //interpolation to get note and title
+        public override string ToString()
+        {
+             return $"[{NoteId}] {Title}";
+        }
+
+
+}
+}
---
+++ ElevenNote.Model/NoteListItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Model
+{
+    public class NoteListItemModel
+    {
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        //interpolation to get note and title
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+
+    }
+}

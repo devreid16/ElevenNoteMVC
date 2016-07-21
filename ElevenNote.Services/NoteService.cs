a89a4e02b5589e4b3cd6d89378f3b01cc64a84f5
Angela Reid <reidang94@gmail.com>
2016-07-21 09:55:29 -0400

    add noteservices controller and views

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,31 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+   public class NoteService
+    {
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var context = new ElevenNoteDBContext())
+            {
+                var query = context.Notes.Select(
+                                e =>
+                                new NoteListItemModel
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                });
+
+                return query.ToArray();
+            }
+        }
+
+    }
+}

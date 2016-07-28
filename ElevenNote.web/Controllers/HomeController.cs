65827dc4ab5177ea3abe765a80a31ff425f56ec1
Angela Reid <reidang94@gmail.com>
2016-07-28 14:26:47 -0400

    add info page and modified name on page

--- ElevenNote.web/Controllers/HomeController.cs
+++ ElevenNote.web/Controllers/HomeController.cs
@@ -20,6 +20,13 @@ namespace ElevenNote.web.Controllers
             return View();
         }
 
+        public ActionResult Info()
+        {
+            ViewBag.Message = "EveryNote Information page.";
+
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
